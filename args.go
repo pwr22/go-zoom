a5e1db1b9583e7d3d2155b0b0609434cbd0f9c88
Peter Roberts <me@peter-r.co.uk>
2019-02-24 04:00:25 +0000

    Add support for :::

--- args.go
+++ args.go
@@ -25,23 +25,100 @@ func parseArgs() {
 	}
 }
 
+// return any command arguments given on the command line, if any
+func getCmdLineArgs() [][]string {
+	var cmdSets [][]string
+
+	inList, start := false, 0
+	for i, a := range flag.Args() {
+		if a == ":::" {
+			if !inList { // this is the first
+				inList = true
+			} else {
+				cmdSets = append(cmdSets, flag.Args()[start:i])
+			}
+
+			start = i + 1 // we always restart counting
+		}
+	}
+
+	if inList && start == len(flag.Args()) { // ::: was the last element
+		fmt.Fprintln(os.Stderr, "::: must be followed by arguments")
+		os.Exit(2)
+	} else if inList {
+		cmdSets = append(cmdSets, flag.Args()[start:len(flag.Args())])
+	}
+
+	return cmdSets
+}
+
+// return the command prefix given on the commandline, if any
+func getCmdPrefix() string {
+	start, end := 0, len(flag.Args())
+
+	for i, a := range flag.Args() {
+		if a == ":::" { // stop as soon as we see any command arguments stuff
+			end = i
+			break
+		}
+	}
+
+	return strings.Join(flag.Args()[start:end], " ")
+}
+
+// returns strings permuting all the argument sets given
+func permuteCmdLineArgSets(sets [][]string) []string {
+	totalCmds := 1
+	for _, cs := range sets {
+		totalCmds *= len(cs)
+	}
+
+	cmds := make([]string, totalCmds)
+	indices := make([]int, len(sets)) // tracks what permutation we're building
+	for jobIdx := 0; jobIdx < totalCmds; jobIdx++ {
+		cmdParts := make([]string, len(sets))
+
+		for cs, i := range indices {
+			cmdParts[cs] = sets[cs][i]
+		}
+		cmds[jobIdx] = strings.Join(cmdParts, " ")
+
+		for cs := range indices {
+			cs = len(indices) - 1 - cs
+
+			if indices[cs] < len(sets[cs])-1 { // if we can do the next component in the current set we're done
+				indices[cs]++
+				break
+			} else { // we need to consider the next component set
+				indices[cs] = 0
+			}
+		}
+	}
+
+	return cmds
+}
+
 // read in commands to run
 func getCmdStrings() []string {
-	// get any command given as args
-	commandPrefix := ""
-	if len(flag.Args()) != 0 {
-		commandPrefix = strings.Join(flag.Args(), " ")
+	cmdPrefix := getCmdPrefix()
+	cmdLineArgSets := getCmdLineArgs() // looks for ::: arguments
+	var cmds []string
+
+	if len(cmdLineArgSets) == 0 { // get args from stdin
+		// one command per line
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Split(bufio.ScanLines)
+
+		// slurp in all the lines
+		for scanner.Scan() {
+			cmds = append(cmds, strings.Join([]string{cmdPrefix, scanner.Text()}, " ")) // prefix with any commands
+		}
+	} else {
+		cmds = permuteCmdLineArgSets(cmdLineArgSets)
+		for i, c := range cmds {
+			cmds[i] = strings.Join([]string{cmdPrefix, c}, " ")
+		}
 	}
 
-	// one command per line
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Split(bufio.ScanLines)
-
-	// slurp in all the lines
-	var lines []string
-	for scanner.Scan() {
-		lines = append(lines, strings.Join([]string{commandPrefix, scanner.Text()}, " ")) // prefix with any commands
-	}
-
-	return lines
+	return cmds
 }
