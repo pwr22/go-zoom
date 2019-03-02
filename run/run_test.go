06ec18860aa63961561512d2e8717b7d0d09cc9a
Peter Roberts <me@peter-r.co.uk>
2019-03-02 19:25:14 +0000

    Add some comments to run test

--- run/run_test.go
+++ run/run_test.go
@@ -4,19 +4,22 @@ import "testing"
 
 var workingCmds = []string{"echo foo", "echo bar"}
 
+// TODO test output
 func TestCmdsImplicitParallelism(t *testing.T) {
 	if exitStatus := Cmds(workingCmds, 0, false); exitStatus != 0 {
 		t.Fatalf("non-zero exit %d", exitStatus)
 	}
 }
 
+// TODO test output
 func TestCmdsExplicitParallelism(t *testing.T) {
 	if exitStatus := Cmds(workingCmds, 1, false); exitStatus != 0 {
 		t.Fatalf("non-zero exit %d", exitStatus)
 	}
 }
 
-func TestCmdsParallelismTooHigh(t *testing.T) {
+// TODO test output
+func TestCmdsParallelismHigherThanJobCount(t *testing.T) {
 	if exitStatus := Cmds(workingCmds, 100, false); exitStatus != 0 {
 		t.Fatalf("non-zero exit %d", exitStatus)
 	}
